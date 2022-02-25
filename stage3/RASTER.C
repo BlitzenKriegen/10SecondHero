96105a2e84aa6238caccd45e50a5ed1e49ad20bb
Enrik <99112423+FrikiEnriki@users.noreply.github.com>
2022-02-25 14:22:25 -0700

    these the stage 2 .c and .h files
    use these sicne they have documentation + fixed naming convention

--- stage3/RASTER.C
+++ stage3/RASTER.C
@@ -2,49 +2,15 @@
 #include <osbind.h>
 #include "raster.h"
 
-#define BITMAP_HEIGHT 16
-#define XBOUNDS 640
-#define YBOUNDS 400
-
-int main(){
-    UINT16 *base = (UINT16*) Physbase();
-    UINT8 *pixel = (UINT8 *) Physbase();
-    UINT16 test_blackout[16]=
-    {
-	0xFFFF,
-	0xFFFF,
-	0xFFFF,
-	0xFFFF,
-	0xFFFF,
-	0xFFFF,
-	0xFFFF,
-	0xFFFF,
-	0xFFFF,
-	0xFFFF,
-	0xFFFF,
-	0xFFFF,
-	0xFFFF,
-	0xFFFF,
-	0xFFFF,
-	0xFFFF
-    };
-
-    int height = BITMAP_HEIGHT;
-    int x;
-    int y;
-	drawHorizontal(base,320,200,test_blackout,height,5);
-	drawVertical(base,220,200,test_blackout,height,5);
-	clearScreen(base);
-	drawPixel(pixel,320,200);
-    return 0;
-}
+/* taken from lab 4*/
 void clearScreen(){
 	printf("\033E\033f\n");     /* blank the screen */
 	return;
 }
+
 /*
 this clearscreen function is relatively slow, just kept
-as a backup
+as a backup 
 void clearScreen(UINT16 *base){
 	int x;
 	int y;
@@ -71,12 +37,13 @@ void clearScreen(UINT16 *base){
 	
 	for(x = 0; x < XBOUNDS; x+=16){
 		for(y = 0; y < YBOUNDS; y+=16){
-			plot_bitmap_16(base,x,y,clr,height);
+			plotBitmap16(base,x,y,clr,height);
 		}
 	}
 	return;
 }
 */
+
 void drawVertical(UINT16 *base,
 			  int x, int y,
 			  const UINT16 *bitmap,
@@ -84,8 +51,8 @@ void drawVertical(UINT16 *base,
 			  int length){
 	int i;
 	for(i = 0; i < length;i++){
-		plot_bitmap_16(base,x,y,bitmap,height);
-		y = y +16;
+		plotBitmap16(base,x,y,bitmap,height);
+		y = y + 16;
 	}
 	return;
 }
@@ -97,8 +64,8 @@ void drawHorizontal(UINT16 *base,
 			  int length){
 	int i;
 	for(i = 0; i < length;i++){
-		plot_bitmap_16(base,x,y,bitmap,height);
-		x = x +16;
+		plotBitmap16(base,x,y,bitmap,height);
+		x = x + 16;
 	}
 	return;
 }
@@ -120,7 +87,7 @@ void plot_bitmap_16(UINT16 *base,
 	
     offset = (x>>4) + (y*40);
     for(i = 0; i < height; i++){
-	*(base + offset + (40*i)) = bitmap[i];
+		*(base + offset + (40*i)) = bitmap[i];
     }
     return;
 }

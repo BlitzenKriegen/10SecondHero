a1f9d13e43f69fdda331fabeafb5048f6d813679
Kiril Sikov <87669848+BlitzenKriegen@users.noreply.github.com>
2022-03-19 15:12:59 -0600

    stub files

---
+++ Stage4/renderer.c
@@ -0,0 +1,31 @@
+#include <stdio>
+#include "renderer.h"
+
+void render_player(const struct Player *Player,UINT16 *base){
+
+	return;
+}
+
+void render_crystal(const struct Crystal *crystal,UINT16 *base){
+	
+	return;
+}
+
+void render_platform(const struct Platform *platform,UINT16 *base){
+	
+	return;
+}
+
+void render_timer(const struct TimeRemaining *timeLeft, UINT16 *base){
+	return;
+}
+
+void render_score(const struct Score *score,UINT16 *base){
+	
+	return;
+}
+
+void render(const struct Model *model, UINT16 *base){
+	
+	return;
+}
---
+++ Stage4/renderer.h
@@ -0,0 +1,13 @@
+#include "model.h"
+
+void render_player(const struct Player *Player,UINT16 *base);
+
+void render_crystal(const struct Crystal *crystal,UINT16 *base);
+
+void render_score(const struct Score *score,UINT16 *base);
+
+void render_timer(const struct TimeRemaining *timeLeft, UINT16 *base);
+
+void render_platform(const struct Platform *platform,UINT16 *base);
+
+void render(const struct Model *model, UINT16 *base);

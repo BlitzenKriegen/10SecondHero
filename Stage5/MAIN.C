225d49e1658f3c2e6ba9dff2f09ba3773d451fa6
Kiril Sikov <87669848+BlitzenKriegen@users.noreply.github.com>
2022-03-23 15:12:16 -0600

    Add files via upload

--- Stage5/MAIN.C
+++ Stage5/MAIN.C
@@ -5,20 +5,33 @@
 #include "model.h"
 
 #define CLOCK 0x462
+#define ESC_ASCII 27
 
 typedef unsigned long ULONG32;
 
 ULONG32 getTime();
 
 int main(){
-	long old_ssp;
 	ULONG32 timeNow;
 	struct Model tenSecondHero;
 	UINT16 *base = Physbase();
+	char tst = NULL;
 
 	timeNow = getTime();
 	render(&tenSecondHero,base);
 	printf("timer: %d\n",timeNow);
+
+	/*For future use as the game state loop.
+	when the user hits the escape button, the game
+	will immediately exit.*/
+	while(tst != ESC_ASCII){
+		if(Cconis()){
+			tst = (char)Cnecin();
+			printf("char input: %d\n",tst);
+			timeNow = getTime();
+			printf("timer: %d\n",timeNow);
+		}
+	}
 
 	return 0;
 }
